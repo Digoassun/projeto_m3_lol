610997ac82672ee3e2d432900bb7d96edec5f458
Josirocha <josi1992@gmail.com>
2022-06-25 19:29:51 -0300

    refatoração do código da consulta

--- kills_por_equipe.sql
+++ kills_por_equipe.sql
@@ -1,15 +1,15 @@
 #CBLOL: qual a equipe com maior numero de Kills?
 SELECT 
     matchinfo.liga,
-    SUBSTRING(kills.assassino FROM 1 FOR 4) AS equipe,
-    COUNT(*) AS numero_de_kills
+    SUBSTRING(kills.assassino FROM 1 FOR 3) AS equipe,
+    count(id_kill) as contagem_mortes 
 FROM
-    kills
+ kills 
         JOIN
-    matchinfo ON kills.assassino LIKE CONCAT(matchinfo.time_azul, '%')
-        OR kills.assassino LIKE CONCAT(matchinfo.time_vermelho, '%')
+    matchinfo ON kills.address_kills = matchinfo.address
 WHERE
     matchinfo.liga = 'CBLol'
-GROUP BY CONCAT(SUBSTRING(kills.assassino FROM 1 FOR 4),
-        '%')
-ORDER BY numero_de_kills DESC;
+GROUP BY equipe
+
+order by contagem_mortes desc
+limit 1;
